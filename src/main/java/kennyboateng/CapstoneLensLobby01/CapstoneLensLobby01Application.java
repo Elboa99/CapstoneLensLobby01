aa8c81b55cec7b431bf7ef4a9b702ec52ab9b3fc
Kenny Boateng <kennyboateng.99@gmail.com>
2024-10-21 02:33:34 +0200

    Initial commit

---
+++ src/main/java/kennyboateng/CapstoneLensLobby01/CapstoneLensLobby01Application.java
@@ -0,0 +1,13 @@
+package kennyboateng.CapstoneLensLobby01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneLensLobby01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneLensLobby01Application.class, args);
+	}
+
+}
---
+++ src/test/java/kennyboateng/CapstoneLensLobby01/CapstoneLensLobby01ApplicationTests.java
@@ -0,0 +1,13 @@
+package kennyboateng.CapstoneLensLobby01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneLensLobby01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
